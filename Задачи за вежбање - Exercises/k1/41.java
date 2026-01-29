71d527f1d1b69efc95607deb204f52b68ffe60db
Filip Iliev <151552696+grga1@users.noreply.github.com>
2026-01-29 06:42:14 +0100

    Add MapSortingTest class for sorting map entries

---
+++ Задачи за вежбање - Exercises/k1/41.java
@@ -0,0 +1,54 @@
+import java.util.*;
+
+public class MapSortingTest {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        scanner.nextLine();
+        List<String> l = readMapPairs(scanner);
+        if(n==1){
+            Map<String, Integer> map = new HashMap<>();
+            fillStringIntegerMap(l, map);
+            System.out.println(map);
+            SortedSet<Map.Entry<String, Integer>> s = entriesSortedByValues(map);
+            System.out.println(s);
+        } else {
+            Map<Integer, String> map = new HashMap<>();
+            fillIntegerStringMap(l, map);
+            System.out.println(map);
+            SortedSet<Map.Entry<Integer, String>> s = entriesSortedByValues(map);
+            System.out.println(s);
+        }
+
+    }
+
+    private static List<String> readMapPairs(Scanner scanner) {
+        String line = scanner.nextLine();
+        String[] entries = line.split("\\s+");
+        return Arrays.asList(entries);
+    }
+
+    static void fillStringIntegerMap(List<String> l, Map<String,Integer> map) {
+        l.stream()
+                .forEach(s -> map.put(s.substring(0, s.indexOf(':')), Integer.parseInt(s.substring(s.indexOf(':') + 1))));
+    }
+
+    static void fillIntegerStringMap(List<String> l, Map<Integer, String> map) {
+        l.stream()
+                .forEach(s -> map.put(Integer.parseInt(s.substring(0, s.indexOf(':'))), s.substring(s.indexOf(':') + 1)));
+    }
+
+   static  <K, V extends Comparable<V>> SortedSet<Map.Entry<K, V>>  entriesSortedByValues(Map<K, V> map){
+          Comparator<Map.Entry<K,V>> comparator = (e1,e2)->{
+              int cmp = e2.getValue().compareTo(e1.getValue());
+              if (cmp!=0){
+                  return cmp;
+              }
+              return 1;
+          };
+       SortedSet<Map.Entry<K, V>> sortedEntries = new TreeSet<>(comparator);
+       sortedEntries.addAll(map.entrySet());
+        return sortedEntries;
+    }
+    //вашиот код овде
+}
